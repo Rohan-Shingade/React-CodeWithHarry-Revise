1b94f0bb6083b5b53842271973e93a1cfea3005c
CarryAllen <rohanshingade1095@gmail.com>
2023-07-29 20:25:54 +0530

    TextForm text summery code modified

--- textutils/src/Components/TextForm.js
+++ textutils/src/Components/TextForm.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import './TextForm.css';
 
 export default function TextForm(props) {
 
@@ -19,18 +20,23 @@ export default function TextForm(props) {
   }
 
   return (
-    <div>
+    <div className='container'>
       <h2 className='my-5'>{props.heading}</h2>
       <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="10"></textarea>
       </div>
 
-      <div className='buttons-area'>
-        <button type="button" className="btn btn-primary mx-3 my-4" onClick={handleUpperCase}>Convert to Uppercase</button>
-        <button type="button" className="btn btn-primary mx-3 my-4" onClick={handleLowerCase}>Convert to Lowercase</button>
-        <button type="button" className="btn btn-primary mx-3 my-4">Copy Text</button>
-        <button type="button" className="btn btn-primary mx-3 my-4">Remove Spaces</button>
-        <button type="button" className="btn btn-primary mx-3 my-4">Delete All</button>
+        <button type="button" className="btn btn-primary my-3" onClick={handleUpperCase}>Convert to Uppercase</button>
+        <button type="button" className="btn btn-primary my-3" onClick={handleLowerCase}>Convert to Lowercase</button>
+        <button type="button" className="btn btn-primary my-3">Copy Text</button>
+        <button type="button" className="btn btn-primary my-3">Remove Spaces</button>
+        <button type="button" className="btn btn-primary my-3">Delete All</button>
+      
+      <div className="container">
+        <h3>Your Text Preview: </h3>
+        <p>{text}</p>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes to read</p>
       </div>
     </div>
   )
