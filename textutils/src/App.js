35012bf758e62224a97cadd2e3e0449be71bbcff
CarryAllen <rohanshingade1095@gmail.com>
2023-07-29 11:30:08 +0530

    uppercase & lowercase button done

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils" aboutText="About Us" contact="Contact Me"/>
+      <div className='container'>
+        <TextForm heading="TextUtils: Word Counter | Character counter | Remove Spaces"/>
+      </div>
     </>
   );
 }
---
+++ textutils/src/Components/TextForm.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+
+  const [text, setText] = useState("Enter your text here");
+
+  const handleOnChange = (event) => {
+    // console.log("on change");
+    setText(event.target.value);
+  }
+
+  const handleUpperCase = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  }
+  const handleLowerCase = () => {
+    let newText2 = text.toLowerCase();
+    setText(newText2);
+  }
+
+  return (
+    <div>
+      <h1 className='my-5'>{props.heading}</h1>
+      <div className="mb-3">
+        <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="10"></textarea>
+      </div>
+
+      <div className='buttons-area'>
+        <button type="button" className="btn btn-primary mx-3 my-4" onClick={handleUpperCase}>Convert to Uppercase</button>
+        <button type="button" className="btn btn-primary mx-3 my-4" onClick={handleLowerCase}>Convert to Lowercase</button>
+        <button type="button" className="btn btn-primary mx-3 my-4">Copy Text</button>
+        <button type="button" className="btn btn-primary mx-3 my-4">Remove Spaces</button>
+        <button type="button" className="btn btn-primary mx-3 my-4">Delete All</button>
+      </div>
+    </div>
+  )
+}
