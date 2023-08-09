b8d8f3e8538ab43557ed791d01dd17eacfb9f38c
CarryAllen <rohanshingade1095@gmail.com>
2023-08-09 17:36:06 +0530

    ...

--- textutils/src/Components/Contact.js
+++ textutils/src/Components/Contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
       <section className="mb-4">
 
           {/* Section heading */}
-          <h2 className="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
+          <h2 className="h1-responsive font-weight-bold text-center my-5">Contact us</h2>
           {/* Section description */}
           <p className="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within a matter of hours to help you.</p>
 
