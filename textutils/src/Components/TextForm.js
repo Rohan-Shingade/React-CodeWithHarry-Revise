f9596ff9ae830df8b3cb7b6647e33b69ce9e7c3f
CarryAllen <rohanshingade1095@gmail.com>
2023-08-14 16:18:42 +0530

    read time edited

--- textutils/src/Components/TextForm.js
+++ textutils/src/Components/TextForm.js
@@ -56,7 +56,7 @@ export default function TextForm(props) {
       <div className="container" style={{color: props.mode === 'dark' ? 'white' : 'black'}}>
         <h3>Your Text Summary </h3>
         <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
-        <p>{0.08 * text.split(" ").length} Minutes to read</p>
+        <p>{0.08 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes to read</p>
         <h4>Text Preview</h4>
         <p>{text.length > 0 ? text:"Nothing to Preview"} </p>
       </div>
