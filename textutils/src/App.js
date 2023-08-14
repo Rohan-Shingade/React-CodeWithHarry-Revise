3d2aabb36a6834880f3603ddb924c5988f200444
CarryAllen <rohanshingade1095@gmail.com>
2023-08-14 12:31:56 +0530

    code updated

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -51,7 +51,7 @@ function App() {
 
       <div className='container'>
         <Routes>
-          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="TextUtils: Word Counter | Character counter | Remove Spaces" mode={mode}/>}/>
+          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="TextUtils: Word Counter | Character counter | Copy Text | Remove Spaces" mode={mode}/>}/>
           <Route exact path="/about" element={<About heading="About TextUtils" mode={mode} />}/>
           <Route path="/*" element={<Error/>}/>
         </Routes>
