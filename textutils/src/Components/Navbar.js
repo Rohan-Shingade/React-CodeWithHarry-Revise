82fecd221b7712de3b78551f4c27cf3ea13417a5
CarryAllen <rohanshingade1095@gmail.com>
2023-08-17 20:12:35 +0530

    Navbar changes done

--- textutils/src/Components/Navbar.js
+++ textutils/src/Components/Navbar.js
@@ -40,7 +40,7 @@ export default function Navbar(props) {
 
             <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
               <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-              <label className="form-check-label mx-2" id='switchLabel' htmlFor="flexSwitchCheckDefault"> Enable {props.mode==='light'?'dark':'light'} mode</label>
+              <label className="form-check-label mx-2 text-center" id='switchLabel' htmlFor="flexSwitchCheckDefault"> Enable {props.mode==='light'?'dark':'light'} mode</label>
             </div>
 
           </div>
